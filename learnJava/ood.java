7411641737c3d91cf0e90ac1e4f1722d57fa4ee5
pranjal-1411 <pranjalguptacse@gmail.com>
2020-07-12 16:17:53 +0530

    Created ood.java 		class gears added

---
+++ learnJava/ood.java
@@ -0,0 +1,15 @@
+package learnJava ;
+
+
+class gears{
+    private int chainring,corgs ;
+    gears( int chainring,int corgs ){ this.chainring = chainring; this.corgs = corgs ; }
+    double ratio(){  return chainring/corgs ;  }
+}
+
+public class ood{
+    public static void main(String[] args){
+
+    }
+}
+
